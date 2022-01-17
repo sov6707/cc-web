55e28f340d3cf0ba7a9e336796e7c543582499bd
rustysov <97886264+rustysov@users.noreply.github.com>
2022-01-17 12:32:45 +0100

    Add installer file

---
+++ src/installer.lua
@@ -0,0 +1,22 @@
+local git_url = "https://raw.githubusercontent.com/rustysov/cc-web/main/src/"
+
+local function download(file, dest)
+    local request = http.get(git_url .. file)
+    local content = request.readAll()
+
+    local f = io.open(dest, "w")
+    f:write(content)
+    f:close()
+    print("Downloaded: " .. file)
+end
+
+local to_download = {
+    "json.lua",
+    "log.lua",
+    "client.lua",
+    "host.lua"
+}
+
+for i, v in pairs(to_download) do
+    download(v, "cc-web/" .. v)
+end
